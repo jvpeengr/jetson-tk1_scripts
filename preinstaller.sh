ce494ce3055aef1c15e52d8f130e514996f5124e
jvpeengr <jvpeengr@gmail.com>
2014-11-16 20:54:21 -0600

    Updated UHD Installers
    run preinstaller.sh before running build-gnuradio

---
+++ preinstaller.sh
@@ -0,0 +1,1 @@
+sudo apt-get -y install libboost-regex1.55.0 libboost-serialization1.55.0 libboost-thread1.55.0 libboost-program-options1.55.0 libboost-system1.55.0 libboost-filesystem1.55.0 libboost-date-time1.55.0 git cmake libcppunit-dev libboost-all-dev libusb-1.0-0-dev python-cheetah doxygen python-docutils build-essential libgsl0-dev
